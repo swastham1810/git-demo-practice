dd762b85a52f21dff17144ee66fe23f3be4f4646
Anki <146679145+Anki5595@users.noreply.github.com>
2024-01-27 15:10:43 +0000

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("My name is Asha");
-        System.out.println("My name is Anki", How are you today?);
+        System.out.println("My name is Anki" How are you today?");
     }
 }
